864b9ee292e188e7e13d636933f6c7664b74d2b8
mami <mamivacc@gmail.com>
2026-07-19 19:12:35 +0300

    fix import

--- src/app/post/actions.ts
+++ src/app/post/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import prisma from "@/lib/prisma";
+import { prisma } from '@/lib/prisma';
 import { revalidatePath } from "next/cache";
 
 // 1. Post Oluşturma (ModernForm.tsx bunu kullanıyor)
